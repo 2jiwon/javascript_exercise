6bb3ce4ea11d913445acd7cd28e307ffab4abd48
2jiwon <ljwjulian@gmail.com>
2018-09-17 20:32:31 +0900

    Finish 006

---
+++ basic/006.js
@@ -0,0 +1,5 @@
+//Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
+//
+year = window.prompt('Input a year: ');
+x = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+window.alert(x);
