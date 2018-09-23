354f942832f19c1607ebd9b9ddd0dfb89ae968cd
2jiwon <ljwjulian@gmail.com>
2018-09-23 22:56:00 +0900

    Finish 077

---
+++ basic/077.js
@@ -0,0 +1,28 @@
+// Write a JavaScript program to test if an array of integers of length 2 contains 1 or a 3. 
+//
+const readline = require('readline');
+const r = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> '
+});
+
+console.log(`Press CTRL + d, when you're done`);
+r.prompt();
+
+let inputArr = [];
+function testArr(arr) {
+  let newArr = [];
+  newArr = arr.map((el, idx, arr) => parseInt(arr[idx]));
+  //console.log(newArr);
+  return (newArr.includes(1) || newArr.includes(3));
+}
+
+r.on('line', (answer) => {
+  r.prompt();
+  inputArr.push(answer);
+});
+
+r.on('close', () => {
+  console.log(testArr(inputArr));
+});
